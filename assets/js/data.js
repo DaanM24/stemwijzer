793dd8aceef4c837f9b8371b1b50bbcc0fd9ad00
Daan <99054517@mydavinci.nl>
2021-04-30 11:49:08 +0200

    verder gewerkt
    ben weer is begonnen

--- assets/js/data.js
+++ assets/js/data.js
@@ -3,128 +3,146 @@ var parties = [{
     name: "VVD",
     secular: true,
     size: 33,
-    long: "Volkspartij voor Vrijheid en Democratie"
+    long: "Volkspartij voor Vrijheid en Democratie",
+    score: 0
   },
   {
     name: "CDA",
     secular: false,
     size: 19,
-    long: "Christen Democratisch Appel"
+    long: "Christen Democratisch Appel",
+    score: 0
   },
   {
     name: "PVV",
     secular: true,
     size: 20,
-    long: "Partij voor de Vrijheid"
+    long: "Partij voor de Vrijheid",
+    score: 0
   },
   {
     name: "D66",
     secular: true,
     size: 19,
-    long: "Democratie 66"
+    long: "Democratie 66",
+    score: 0
   },
   {
     name: "GroenLinks",
     secular: true,
-    size: 14
+    size: 14,
+    score: 0
   },
   {
     name: "SP",
     secular: true,
     size: 14,
-    long: "Socialistische Partij"
+    long: "Socialistische Partij",
+    score: 0
   },
   {
     name: "PvdA",
     secular: true,
     size: 9,
-    long: "Partij van de Arbeid"
+    long: "Partij van de Arbeid",
+    score: 0
   },
   {
     name: "ChristenUnie",
     secular: false,
-    size: 5
+    size: 5,
+    score: 0
   },
   {
     name: "Partij voor de Dieren",
     secular: true,
-    size: 33
+    size: 33,
+    score: 0
   },
   {
     name: "SGP",
     secular: false,
     size: 33,
-    long: "Staatkundig Gerefomeerde Partij"
+    long: "Staatkundig Gerefomeerde Partij",
+    score: 0
   },
   {
     name: "DENK",
     secular: true,
-    size: 3
+    size: 3,
+    score: 0
   },
   {
     name: "Forum voor Democratie",
     secular: true,
-    size: 2
+    size: 2,
+    score: 0
   },
   {
     name: "Lokaal in de Kamer",
     secular: true,
-    size: 0
+    size: 0,
+    score: 0
   },
   {
     name: "OndernemersPartij",
     secular: true,
-    size: 0
+    size: 0,
+    score: 0
   },
   {
     name: "VNL",
     secular: true,
-    size: 0
+    size: 0,
+    score: 0
   },
   {
     name: "Nieuwe Wegen",
     secular: true,
-    size: 0
+    size: 0,
+    score: 0
   },
   {
     name: "De Burger Beweging",
     secular: true,
-    size: 0
+    size: 0,
+    score: 0
   },
   {
     name: "Piratenpartij",
     secular: true,
-    size: 0
+    size: 0,
+    score: 0
   },
   {
     name: "Artikel 1",
     secular: true,
-    size: 0
-  },
-  {
-    name: "Libertarische Partij",
-    secular: true,
-    size: 0
+    size: 0,
+    score: 0
   },
   {
     "name": "50Plus",
     "secular": true,
-    "size": 2
+    "size": 2,
+    score: 0
   },
   {
     "name": "Vrijzinnige Partij",
     "secular": true,
-    "size": 0
+    "size": 0,
+    score: 0
   },
   {
     "name": "Libertarische Partij",
     "secular": true,
-    "size": 0
+    "size": 0,
+    score: 0
   },
   {
     "name": "Niet Stemmers",
     "secular": true,
-    "size": 0
+    "size": 0,
+    score: 0
   }
 
 ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,53 +2,76 @@ var startKnop = document.getElementById("startKnop");
 var idkKnop = document.getElementById("idkKnop");
 var oneensKnop = document.getElementById("oneensKnop");
 var eensKnop = document.getElementById("eensKnop");
+var skipKnop = document.getElementById("skipKnop");
 var title = document.getElementById("title");
 var description = document.getElementById("description");
 var x = 0;
-var antwoorden = []
+var antwoorden = [];
 
 idkKnop.style.display = 'none';
 oneensKnop.style.display = 'none';
 eensKnop.style.display = 'none';
+skipKnop.style.display = 'none';
+
 
 function start(){
-    startKnop.innerHTML = "Vorige";
+    startKnop.innerHTML = "VORIGE";
     idkKnop.style.display = 'inline';
     oneensKnop.style.display = 'inline';
     eensKnop.style.display = 'inline';
-    title.innerHTML = subjects[x]['title'];
-    description.innerHTML = subjects[x]['statement'];
+    skipKnop.style.display = 'inline';
+    title.innerHTML = subjects[x].title;
+    description.innerHTML = subjects[x].statement;
     if(x > 0){
         x--
     }
 }
 
 function eens(){
-    title.innerHTML = subjects[x]['title'];
-    description.innerHTML = subjects[x]['statement'];
     antwoorden[x] = "pro";
-    x++;
-    if(x >= 30){
-        document.write("test");
-    };
+    goNextStatement();
 }
 
 function oneens(){
-    title.innerHTML = subjects[x]['title'];
-    description.innerHTML = subjects[x]['statement'];
     antwoorden[x] = "contra";
-    x++;
-    if(x >= 30){
-        document.write("test");
-    };
+    goNextStatement();
 }
 
 function idk(){
-    title.innerHTML = subjects[x]['title'];
-    description.innerHTML = subjects[x]['statement'];
-    antwoorden[x] = "idk";
+    antwoorden[x] = "none";
+    goNextStatement();
+}
+
+function skip(){
+    antwoorden[x] = "undefined";
+    goNextStatement();
+}
+
+function goNextStatement(){
     x++;
-    if(x >= 30){
-        document.write("test");
-    };
+    if(x >= subjects.length){
+        compareStatement();
+        showResult();
+    }else{
+        title.innerHTML = subjects[x].title;
+        description.innerHTML = subjects[x].statement;
+    }
+}
+
+function compareStatement(){
+    for(b = 0; b < subjects.length; b++){
+        for(a = 0; a < parties.length; a++){
+            if(antwoorden[b] == subjects[b].parties[a].position){
+                parties[a].score++;
+            }else if(antwoorden[b] == "undefined"){
+                parties[a].score + 0;
+            }
+        };
+    }
+}
+
+function showResult(){
+    for(c = 0; c < parties.length; c++){
+        document.write(parties[c].name, " ", parties[c].score+ "<br>");
+    }
 }
